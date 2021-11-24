effd8e08d28774c0e8d951015e3fa7f421598bed
John-Williams <jcwilliams@gmail.com>
2021-11-24 00:02:20 +0000

    day 7

--- 2020/7/main.py
+++ 2020/7/main.py
@@ -3,7 +3,7 @@ import re
 
 input_lines = data.input().splitlines()
 
-#input_lines = ["pale chartreuse bags contain 3 faded orange bags.", "posh black bags contain 3 dark lavender bags, 3 mirrored coral bags, 1 dotted chartreuse bag."]
+#input_lines = ["pale chartreuse bags contain 3 shiny gold bags.", "posh black bags contain 3 pale chartreuse bags, 3 mirrored coral bags, 1 dotted chartreuse bag."]
 
 bag_rules = []
 
@@ -17,32 +17,37 @@ for line in input_lines:
 
     rest_of_string = line[len(first_part_of_string):].rstrip('.')
 
-    #print(f"first_bag: {first_bag_name} rest of string: {rest_of_string}")
-
     contains_bags = re.findall('(\d+)\s(\w+\s\w+)\s', rest_of_string)
 
-    #print(contains_bags)
     bag_rules.append((first_bag_name, contains_bags))
-
-#print(bag_rules)
-#print(dict(bag_rules[0][1]).values())
 
 
 def get_parents(bag_name: str):
-    return list(filter(lambda rule:  bag_name in dict(rule[1]).values(), bag_rules))
+    return list(filter(lambda rule:  bag_name in [r[1] for r in rule[1]], bag_rules))
 
-parent_count = 0
+all_parents_names = []
 
-
-#recursive func goes here
-
-while True:
-    parents = get_parents('shiny gold')
-    if (len(parents) == 0):
-        break
-    parent_count += len(parents)
-    for parent in parents
+def find_all(parent_name: str):
+    parents = get_parents(parent_name)    
+    all_parents_names.extend([parent[0] for parent in parents])    
+    
+    for parent in parents:
+        find_all(parent[0])
 
 
+find_all("shiny gold")
 
-print(parents)
+distinct_parents = set(all_parents_names)
+
+print(f"part one: {len(distinct_parents)}")
+
+#------------ part 2. not complete.
+
+bag_count = 0
+for parent_name in distinct_parents:
+    children = [bag_rule[1] for bag_rule in bag_rules if bag_rule[0] == parent_name]
+    bag_count = bag_count + 1 + sum([int(child[0]) for child in children[0]]) #this needs work
+
+print(f"part two: {bag_count}")
+
+
